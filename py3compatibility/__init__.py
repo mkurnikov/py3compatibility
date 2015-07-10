e7057fecddfd0f5e5680558daf816732fefaed20
mkurnikov <maxim.kurnikov@gmail.com>
2015-07-10 15:16:31 +0300

    initial commit

---
+++ py3compatibility/__init__.py
@@ -0,0 +1,19 @@
+# __future__ imports are local to module imported it
+from __future__ import division
+from __future__ import print_function
+
+try:
+    from future_builtins import *
+except ImportError:
+    pass
+
+try:
+    input = raw_input
+    # range = xrange
+except NameError:
+    pass
+
+
+import resource
+soft, hard = 2 ** 32, 2 ** 32 #4 gigabyte
+resource.setrlimit(resource.RLIMIT_AS,(soft, hard))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='py3compatibility',
+    version='0.0.1.dev1',
+    author="Maxim Kurnikov",
+    author_email="maxim.kurnikov@gmail.com",
+    packages=find_packages()
+)
+
+
