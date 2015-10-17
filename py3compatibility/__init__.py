12b6891ab11175ca9571f2a83f794d993e04287f
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2015-10-17 17:34:41 +0300

    final

--- py3compatibility/__init__.py
+++ py3compatibility/__init__.py
@@ -9,11 +9,12 @@ except ImportError:
 
 try:
     input = raw_input
-    # range = xrange
+    range = xrange
 except NameError:
     pass
 
 
 import resource
 soft, hard = 2 ** 32, 2 ** 32 #4 gigabyte
+#soft, hard = 2 * soft, 2 * hard
 resource.setrlimit(resource.RLIMIT_AS,(soft, hard))
